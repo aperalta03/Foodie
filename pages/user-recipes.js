0df60423e60826e0b9694336c4e379b9304914f0
aperalt03 <aperalta03@tamu.edu>
2024-09-06 10:46:47 -0500

    Minor Mobile Changes

--- pages/user-recipes.js
+++ pages/user-recipes.js
@@ -15,7 +15,7 @@ const UserRecipes = () => {
   const [recipes, setRecipes] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredRecipes, setFilteredRecipes] = useState([]);
-  const [isMobileView, setIsMobileView] = useState(false); // New state for mobile view
+  const [isMobileView, setIsMobileView] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
@@ -50,14 +50,8 @@ const UserRecipes = () => {
         setIsMobileView(false);
       }
     };
-
-    // Initial check
     handleResize();
-
-    // Add resize listener
     window.addEventListener('resize', handleResize);
-
-    // Cleanup the listener
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
@@ -129,7 +123,7 @@ const UserRecipes = () => {
               {filteredRecipes.map((recipe, index) => (
                 <Box key={index} className={styles.pdfContainer}>
                   <iframe   
-                    src={`${recipe}${isMobileView ? '#toolbar=0&zoom=75' : '#toolbar=0'}`}  // Conditional zoom
+                    src={`${recipe}${isMobileView ? '#toolbar=0&zoom=75' : '#toolbar=0'}`}
                     title={`Recipe ${index + 1}`}
                     className={styles.pdfFrame}
                   />
